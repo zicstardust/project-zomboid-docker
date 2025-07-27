d119189e65cd5e100c9688d766deb7e3a4d5384d
Zic <git@zicstardust.com>
2025-07-27 12:25:35 -0400

    change variables names

--- run.sh
+++ run.sh
@@ -3,21 +3,10 @@
 cd /home/steam/pz_server
 
 #Set non-steam
-#if [[ -z "${NON_STEAM}" ]]; then
-if [ "$NON_STEAM" == "true" ]; then
-    STEAM_CONFIG="0"
-else
-    STEAM_CONFIG="1"
-fi
-sed -i "s/-Dzomboid.steam=1/-Dzomboid.steam=${STEAM_CONFIG}/" ProjectZomboid64.json
+sed -i "s/-Dzomboid.steam=1/-Dzomboid.steam=${STEAM:-1}/" ProjectZomboid64.json
 
 #Set Max RAM
-if [[ -z "${MAX_RAM}" ]]; then
-    RAM="8g"
-else
-    RAM=${MAX_RAM}
-fi
-sed -i "s/-Xmx8g/-Xmx${RAM}/" ProjectZomboid64.json
+sed -i "s/-Xmx8g/-Xmx${MAX_RAM:-8g}/" ProjectZomboid64.json
 
 #server run
 RANDOM_PASSWORD=$(head -c 16 /dev/urandom | base64 | tr -dc 'a-zA-Z0-9')
