6c100ff6159c8c0b7d3a4d3129005d2782fb5b7c
Zic <git@zicstardust.com>
2025-08-12 21:04:59 -0400

    remove 32bit edit files

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@
 cd /app
 
 #Set non-steam
-sed -i "s/-Dzomboid.steam=1/-Dzomboid.steam=${STEAM:-1}/" {ProjectZomboid32.json,ProjectZomboid64.json}
+sed -i "s/-Dzomboid.steam=1/-Dzomboid.steam=${STEAM:-1}/" ProjectZomboid64.json
 
 #Set Max RAM
-sed -i "s/-XmxTEMP/-Xmx${MAX_RAM:-4g}/" ProjectZomboid64.json {ProjectZomboid32.json,ProjectZomboid64.json}
+sed -i "s/-XmxTEMP/-Xmx${MAX_RAM:-4g}/" ProjectZomboid64.json
 
 #Set language
-sed -i "s/-Duser.language=TEMP/-Duser.language=${LANGUAGE:-en}/" ProjectZomboid64.json {ProjectZomboid32.json,ProjectZomboid64.json}
+sed -i "s/-Duser.language=TEMP/-Duser.language=${LANGUAGE:-en}/" ProjectZomboid64.json
 
 #server run
 RANDOM_PASSWORD=$(head -c 16 /dev/urandom | base64 | tr -dc 'a-zA-Z0-9')
