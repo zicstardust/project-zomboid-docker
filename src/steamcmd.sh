e545c6223140b4d3c0ae0e9df02b796a4f04582f
Zic <git@zicstardust.com>
2026-03-07 07:46:27 -0400

    refactor steamcmd.sh: clean up error message formatting and remove unused code

--- src/steamcmd.sh
+++ src/steamcmd.sh
@@ -4,7 +4,6 @@ current_dir=$(pwd)
 
 cd /opt/steamcmd
 
-
 while true; do
     ./steamcmd.sh "$@"
     #echo "Steamcmd return code: $?"
@@ -12,12 +11,8 @@ while true; do
     if [ "$return_code" == "0" ]; then
         break
     else
-        echo "Steamcmd error: ${return_code}, try again"
+        echo "Steamcmd error ${return_code}, try again"
     fi  
 done
 
-#return_code=$?
-
 cd $current_dir
-
-#exit $return_code
